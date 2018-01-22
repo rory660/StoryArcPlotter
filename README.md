27c78b61b16bc71bbcb7d72e10d6b7070c33ebe7
Rory Brown <rorybrown660@gmail.com>
2018-01-22 08:32:02 +0000

    Refactored code into functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # StoryArcPlotter
-Graphs the level of emotion over time in a piece of literature to help visualise the story arc.
+Graphs the level of emotion over time in a piece of literature to help visualise the story arc.  
+
+Specifically designed for use with the Shakespeare plays hosted at [shakespeare.mit.edu](http://shakespeare.mit.edu/)
--- analysis.py
+++ analysis.py
@@ -3,91 +3,94 @@ import re
 import string
 import matplotlib.pyplot as plt
 
-sentiment = {}
+def getSentimentData(filename):
+	sentiment = {}
+	with open (filename , "r") as sentimentData:
+		for item in [x.split("	") for x in sentimentData]:
+			sentiment[item[0]] = item[1].replace("\n" , "")
+	return sentiment
 
-with open ("AFINN-111.txt" , "r") as sentimentData:
-	for item in [x.split("	") for x in sentimentData]:
-		sentiment[item[0]] = item[1].replace("\n" , "")
+def loadPage(url):
+	page = str(request.urlopen(url).read())
+	page = page.split("\\n")
 
-print (sentiment["abandon"])
+	page = [y for y in [re.sub('<[^<]+?>', '', x) for x in page] if y != "" and len(y) > 1]
+	page = page[page.index("ACT I"):]
+	return page
 
-page = str(request.urlopen("http://shakespeare.mit.edu/macbeth/full.html").read())
-page = page.split("\\n")
+def calculatePageSentiment(page, sentiment, window = 100):
+	pageSentiment = []
+	index = window//2
+	while index < len(page)-window//2:
+		windowSentiment = 0
+		wordCount = 0
+		for i in range(index - window//2,index + window//2):
+			for word in [x.lower() for x in page[i].split()]:
+				for char in ",./:;'?\\&":
+					word = word.replace(char, "")
+				if word in sentiment.keys():
+					wordCount+=1
+					windowSentiment += int(sentiment[word])
+		if wordCount == 0:
+			wordCount = 1
+		pageSentiment.append(windowSentiment/wordCount / window)
+		index+=1
+	return pageSentiment
 
-page = [y for y in [re.sub('<[^<]+?>', '', x) for x in page] if y != "" and len(y) > 1]
-page = page[page.index("ACT I"):]
+def ScaleData(pageSentiment, stepModifier = 200):
+	global step
+	pageSentiment2 = []
+	index = 0
+	step = int(len(pageSentiment)/stepModifier)
+	while index < len(pageSentiment) - step:
+		pageSentiment2.append(sum(pageSentiment[index:index+step])/len(pageSentiment[index:index+step]))
+		index += step
 
-pageSentiment = []
+	return pageSentiment2
 
-# for line in page:
-# 	lineSentiment = 0
-# 	wordCount = 0
-# 	for word in [x.lower() for x in line.split()]:
-# 		for char in ",./:;'?\\&":
-# 			word = word.replace(char, "")
-# 		if word in sentiment.keys():
-# 			wordCount += 1
-# 			lineSentiment += int(sentiment[word])
-# 	if wordCount == 0:
-# 		wordCount = 1
-# 	pageSentiment.append(lineSentiment/wordCount)
+def calculateCumulativePageSentiment(pageSentiment):
+	global step
+	pageSentimentCumulative = [pageSentiment[0]]
+	for num in pageSentiment:
+		pageSentimentCumulative.append(pageSentimentCumulative[-1]+num*step)
 
+	return pageSentimentCumulative
 
-window = 100
-index = window//2
-while index < len(page)-window//2:
-	windowSentiment = 0
-	wordCount = 0
-	for i in range(index - window//2,index + window//2):
-		for word in [x.lower() for x in page[i].split()]:
-			for char in ",./:;'?\\&":
-				word = word.replace(char, "")
-			if word in sentiment.keys():
-				wordCount+=1
-				windowSentiment += int(sentiment[word])
-	if wordCount == 0:
-		wordCount = 1
-	pageSentiment.append(windowSentiment/wordCount / window)
-	index+=1
+def plotData(pageSentiment, pageSentiment2):
 
-pageSentiment3 = []
+	if (max(pageSentiment) > -min(pageSentiment)):
+		yScale = max(pageSentiment)*1.1
+	else:
+		yScale = -min(pageSentiment)*1.1
 
-index = 0
-step = int(len(pageSentiment)/200)
-while index < len(pageSentiment) - step:
-	pageSentiment3.append(sum(pageSentiment[index:index+step])/len(pageSentiment[index:index+step]))
-	index += step
-print (pageSentiment3)
+	if (max(pageSentiment2) > -min(pageSentiment2)):
+		yScale2 = max(pageSentiment2)*1.1
+	else:
+		yScale2 = -min(pageSentiment2)*1.1
 
-pageSentiment = pageSentiment3
+	average2 = sum(pageSentiment2)/len(pageSentiment2)
+	if max(pageSentiment2) - average2 > average2 - min(pageSentiment2):
+		yScale2 = (max(pageSentiment2) - average2) * 1.1
+	else:
+		yScale2 = (average2 - min(pageSentiment2)) * 1.1
+	plt.subplot(2,1,1)
+	plt.plot(range(0,len(pageSentiment2)*step,step),pageSentiment2,"-")
+	plt.title("Average Sentiment per Line")
+	plt.ylabel("Cumulative")
+	plt.ylim([average2 - yScale2 ,average2 + yScale2])
+	plt.subplot(2,1,2)
+	plt.plot(range(0,len(pageSentiment)*step,step),pageSentiment,"-")
+	plt.xlabel("Line Number")
+	plt.ylabel("Individual")
+	plt.ylim([-yScale,yScale])
+	plt.show()
 
-pageSentiment2 = [pageSentiment[0]]
-for num in pageSentiment:
-	pageSentiment2.append(pageSentiment2[-1]+num*step)
+def analysePlay(url, sentimentFilepath):
+	sentimentDict = getSentimentData(sentimentFilepath)
+	play = loadPage(url)
+	playSentiment = calculatePageSentiment(play, sentimentDict)
+	playSentiment = ScaleData(playSentiment)
+	playSentimentCumulative = calculateCumulativePageSentiment(playSentiment)
+	plotData(playSentiment, playSentimentCumulative)
 
-if (max(pageSentiment) > -min(pageSentiment)):
-	yScale = max(pageSentiment)*1.1
-else:
-	yScale = -min(pageSentiment)*1.1
-
-if (max(pageSentiment2) > -min(pageSentiment2)):
-	yScale2 = max(pageSentiment2)*1.1
-else:
-	yScale2 = -min(pageSentiment2)*1.1
-
-average2 = sum(pageSentiment2)/len(pageSentiment2)
-if max(pageSentiment2) - average2 > average2 - min(pageSentiment2):
-	yScale2 = (max(pageSentiment2) - average2) * 1.1
-else:
-	yScale2 = (average2 - min(pageSentiment2)) * 1.1
-plt.subplot(2,1,1)
-plt.plot(range(0,len(pageSentiment2)*step,step),pageSentiment2,"-")
-plt.title("Average Sentiment per Line")
-plt.ylabel("Cumulative")
-plt.ylim([average2 - yScale2 ,average2 + yScale2])
-plt.subplot(2,1,2)
-plt.plot(range(0,len(pageSentiment)*step,step),pageSentiment,"-")
-plt.xlabel("Line Number")
-plt.ylabel("Individual")
-plt.ylim([-yScale,yScale])
-plt.show()
+analysePlay("http://shakespeare.mit.edu/macbeth/full.html", "AFINN-111.txt")
