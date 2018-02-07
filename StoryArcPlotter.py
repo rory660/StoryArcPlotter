ad957bcef9d810d188628191d52ef4e391b215dd
Rory Brown <rorybrown660@gmail.com>
2018-02-07 14:03:48 +0000

    Removed useless graph from figure

--- StoryArcPlotter.py
+++ StoryArcPlotter.py
@@ -3,6 +3,7 @@ import re
 import string
 import matplotlib.pyplot as plt
 import sys
+import csv
 
 def getSentimentData(filename):
 	print("Importing sentiment Data...")
@@ -24,22 +25,36 @@ def loadPage(url):
 def calculatePageSentiment(page, sentiment, window = 100):
 	print("Analysing Play...")
 	pageSentiment = []
-	index = window//2
-	while index < len(page)-window//2:
-		windowSentiment = 0
+	index = 0
+	for line in page:
+		lineSentiment = 0
 		wordCount = 0
-		for i in range(index - window//2,index + window//2):
-			for word in [x.lower() for x in page[i].split()]:
-				for char in ",./:;'?\\&":
-					word = word.replace(char, "")
+		for word in [x.lower() for x in line.split()]:
+			for char in ",./:;'?\\&":
+				word = word.replace(char, "")
 				if word in sentiment.keys():
-					wordCount+=1
-					windowSentiment += sentiment[word]
+					wordCount +=1
+					lineSentiment += sentiment[word]
 		if wordCount == 0:
 			wordCount = 1
-		pageSentiment.append(windowSentiment/wordCount / window)
-		index+=1
-	return pageSentiment
+		pageSentiment.append(lineSentiment/wordCount)
+
+	pageSentiment2 = [sum(pageSentiment[:window])]
+
+	while index < len(pageSentiment):
+		if index - window/2-1 < 0:
+			remove = 0
+		else:
+			remove = pageSentiment[index-int(window/2)-1]
+		if index + window/2 + 1 > len(pageSentiment)-1:
+			add = 0
+		else:
+			add = pageSentiment[index+int(window/2)+1]
+		pageSentiment2.append(pageSentiment2[-1]+add-remove)
+		index +=1
+	pageSentiment2 = [x/window for x in pageSentiment2]
+
+	return pageSentiment2
 
 def ScaleData(pageSentiment, stepModifier = 200):
 	global step
@@ -52,25 +67,10 @@ def ScaleData(pageSentiment, stepModifier = 200):
 
 	return pageSentiment2
 
-def calculateCumulativePageSentiment(pageSentiment):
-	global step
-	pageSentimentCumulative = [pageSentiment[0]]
-	for num in pageSentiment:
-		pageSentimentCumulative.append(pageSentimentCumulative[-1]+num*step)
-
-	return pageSentimentCumulative
-
-def plotData(pageSentiment, pageSentiment2, playName):
+def plotData(pageSentiment, playName):
 	print("Plotting Data...")
-
 	average = sum(pageSentiment)/len(pageSentiment)
-	average2 = sum(pageSentiment2)/len(pageSentiment2)
-	plt.subplot(2,1,1)
-	plt.plot(range(0,len(pageSentiment2)*step,step),[100*((x+1)/(max(pageSentiment2)+1)) for x in pageSentiment2],"-")
 	plt.title("Relative Happiness Level: "+playName)
-	plt.ylabel("Happiness % (Cumulative)")
-	plt.ylim([-5,105])
-	plt.subplot(2,1,2)
 	plt.plot(range(0,len(pageSentiment)*step,step),[100*((x+1)/(max(pageSentiment)+1)) for x in pageSentiment],"-")
 	plt.xlabel("Line Number")
 	plt.ylabel("Happiness %")
@@ -83,24 +83,18 @@ def averageData(pageSentiment):
 		return [x/max(pageSentiment) for x in pageSentiment]
 	else:
 		return [x/(-min(pageSentiment)) for x in pageSentiment]
-	
-def averageDataCumulative(pageSentiment):
-	pageSentiment2 = pageSentiment[0]
-	pageSentiment2 = [sum(pageSentiment2)+x for x in pageSentiment[1:]]
-	if max(pageSentiment2) > min(pageSentiment2):
-		return [x/max(pageSentiment2) for x in pageSentiment2]
-	else:
-		return [x/(-min(pageSentiment2)) for x in pageSentiment2]
 
 def analysePlay(url, sentimentFilepath, playName):
 	sentimentDict = getSentimentData(sentimentFilepath)
 	play = loadPage(url)
 	playSentiment = calculatePageSentiment(play, sentimentDict, window = 500)
 	playSentiment = ScaleData(playSentiment)
-	playSentimentCumulative = calculateCumulativePageSentiment(playSentiment)
 	playSentiment = averageData(playSentiment)
-	playSentimentCumulative = averageData(playSentimentCumulative)
-	plotData(playSentiment, playSentimentCumulative, playName)
+	if (input("Output data as a CSV file? y/n\n>").lower().strip() == "y"):
+		writeCSV(playSentiment, playName)
+	if (input("Generate a Graph? y/n\n>").lower().strip() == "y"):
+		plotData(playSentiment, playName)
+	
 
 def getPlayLinks():
 	page = str(request.urlopen("http://shakespeare.mit.edu/").read())
@@ -130,10 +124,13 @@ def ui():
 		except:
 			print("Enter a number corresponding to your play choice.")
 	analysePlay(list(links.values())[choice-1],"AFINN-111.txt",list(links.keys())[choice-1])
+
+def writeCSV(playSentiment, name):
+	with open (name+".csv","w") as writeFile:
+		writer = csv.writer(writeFile)
+		writer.writerow(["Line Number","Happiness Level"])
+		line = 0
+		for row in playSentiment:
+			line+=1
+			writer.writerow([line,int(row*10**5)/10**5])
 ui()
-# if len(sys.argv) > 1 and "shakespeare.mit.edu" in sys.argv[1]:
-# 	analysePlay(sys.argv[1], "AFINN-111.txt")
-# else:
-# 	print("Please enter the url of a shakespeare play in the format: http://shakespeare.mit.edu/[Play Title]/full.html")
-# 	playUrl = input("> ")
-# 	analysePlay(playUrl, "AFINN-111.txt")
