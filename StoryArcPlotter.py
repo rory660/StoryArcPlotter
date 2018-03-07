7264d1ee99dbccedc82a0c1efbc0093fd31ad9dc
Rory Brown <rorybrown660@gmail.com>
2018-03-07 15:53:25 +0000

    Added license info.

--- StoryArcPlotter.py
+++ StoryArcPlotter.py
@@ -1,3 +1,21 @@
+# StoryArcPlotter.py - Generates graphs based on the happiness of a shakespeare play per line.
+# Copyright (C) 2018 Rory Brown <rorybrown660@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This program makes use of the AFINN sentiment dataset by Finn Årup Nielsen, under the "Open Database License (ODbL) v1.0"<http://www.opendatacommons.org/licenses/odbl/1.0/>.
+
 import urllib.request as request
 import re
 import string
